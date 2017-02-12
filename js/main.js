e2fef408321377ea8c8702fb41a47a1f40502147
Hari Narayanan <smgdark@gmail.com>
2017-02-12 17:25:52 +0530

    Made minor fixes and added cdn

--- js/main.js
+++ js/main.js
@@ -3,102 +3,44 @@
 var App = {};
 
 App = {
-
-    /*
-    * Global variables used throughout the app
-    * */
-    GLOBAL: {
-        activeToasts: 0,
-        $pace: $('.pace'),
-        progress: {
-            show: function show () {
-                App.GLOBAL.$pace.show().css('visibility', 'visible');
-            },
-            hide: function hide () {
-                App.GLOBAL.$pace.hide().css('visibility', 'hidden');
-            }
-        },
-        isTouch: function isTouch () {
-            return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
-        },
-        $nav: $('nav')
-    },
-
-    /*
-    * Loader handling
-    * */
-    onLoad: function onLoad () {
-        window.onload = function () {
-            $('#loading-overlay').fadeOut();
-            App.GLOBAL.progress.hide();
-        };
-    },
-
-    /*
-     * Initing side nav for medium and smaller devices
-     * */
-    initSideNav: function sinitSideNav () {
-        $('#sidenav-toggle').sideNav({
-            closeOnClick: true
-        });
-    },
-
-    /*
-    * Handling nav color scheme, depending on scroll position
-    * */
-    initNavColorChange: function initNavColorChange () {
-        var $trianglesHeight = $('#triangles').height() || 720;
-        $(document).on('scroll ready', function () {
-            App._switchNavColorSchemes($trianglesHeight);
-        });
-    },
-
-    /*
-    * A private function that toggles .dark class for nav element
-    * */
-    _switchNavColorSchemes: function _switchNavColorSchemes ($trianglesHeight) {
-        if ($(window).scrollTop() > $trianglesHeight) {
-            App.GLOBAL.$nav.addClass('dark');
-        } else {
-            App.GLOBAL.$nav.removeClass('dark');
-        }
-    },
-
-    /*
-    * Initialising scroll spy to detect current section
-    * Check it out here: http://materializecss.com/scrollspy.html
-    * */
-    initScrollSpy: function initScrollSpy () {
-        $('.scrollspy').scrollSpy({
-            scrollOffset: 50
-        });
-    },
-
-    /*
-    * Initialising parallax
-    * http://materializecss.com/parallax.html
-    * */
-    initParallax: function initParallax () {
-        $('.parallax').parallax();
-    },
-
-    /*
-    * Initialising slider
-    * http://materializecss.com/media.html#slider
-    * */
-    initSlider: function initSlider () {
-        $('.slider').slider({
-            full_width: true,
-            height: 720
-        });
-    }
+	GLOBAL: {
+		activeToasts: 0,
+		isTouch: function isTouch() {
+			return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+		},
+		$nav: $('nav')
+	},
+	initSideNav: function sinitSideNav() {
+		$('#sidenav-toggle').sideNav({
+			closeOnClick: true
+		});
+	},
+	initNavColorChange: function initNavColorChange() {
+		var $trianglesHeight = $('#triangles').height() || 720;
+		$(document).on('scroll ready', function () {
+			App._switchNavColorSchemes($trianglesHeight);
+		});
+	},
+	_switchNavColorSchemes: function _switchNavColorSchemes($trianglesHeight) {
+		if ($(window).scrollTop() > $trianglesHeight) {
+			App.GLOBAL.$nav.addClass('dark');
+		} else {
+			App.GLOBAL.$nav.removeClass('dark');
+		}
+	},
+	initScrollSpy: function initScrollSpy() {
+		$('.scrollspy').scrollSpy({
+			scrollOffset: 50
+		});
+	},
+	initParallax: function initParallax() {
+		$('.parallax').parallax();
+	}
 };
 
 $(document).on('ready', function () {
-    App.onLoad();
-    App.initSideNav();
-    App.initNavColorChange();
-    App.initScrollSpy();
-    App.initParallax();
-    App.initSlider();
+	App.initSideNav();
+	App.initNavColorChange();
+	App.initScrollSpy();
+	App.initParallax();
 });
